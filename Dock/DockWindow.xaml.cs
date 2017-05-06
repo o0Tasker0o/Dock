30b1712c4d7e4390ba4d9ffe2f8750bf658e3a9f
James Tasker <james_tasker1@hotmail.com>
2017-05-06 15:15:01 +0100

    Add initial project

---
+++ Dock/DockWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace Dock
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class DockWindow
+	{
+		public DockWindow()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnInitialized(EventArgs e)
+		{
+			var screenWidth = SystemParameters.PrimaryScreenWidth;
+			Left = (screenWidth / 2) - (Width / 2);
+			Top = 0;
+
+			base.OnInitialized(e);
+		}
+	}
+}
