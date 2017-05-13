820c90343adcbf3dc4dc6d19ec45976a42d3ca1e
James Tasker <james_tasker1@hotmail.com>
2017-05-13 16:11:30 +0100

    Improve image rendering

--- Dock/DockWindow.xaml.cs
+++ Dock/DockWindow.xaml.cs
@@ -16,10 +16,8 @@ namespace Dock
 	/// </summary>
 	public partial class DockWindow
 	{
-		private readonly TimeSpan _swellDuration = TimeSpan.FromMilliseconds(100);
 		private readonly TimeSpan _popupDuration = TimeSpan.FromMilliseconds(100);
-		private readonly Thickness _defaultPadding = new Thickness(6, 6, 6, 6);
-		private readonly Thickness _mouseOverPadding = new Thickness(3, 3, 3, 3);
+		private const int HiddenPosition = -72;
 
 		public DockWindow()
 		{
@@ -34,7 +32,7 @@ namespace Dock
 
 			var screenWidth = SystemParameters.PrimaryScreenWidth;
 			Left = (screenWidth / 2) - (Width / 2);
-			Top = -64;
+			Top = HiddenPosition;
 
 			base.OnInitialized(e);
 		}
@@ -85,11 +83,12 @@ namespace Dock
 						BlurRadius = 10.0,
 						Opacity = 0.0,
 						ShadowDepth = 0.0
-					}
+					},
+					Width = 64,
+					Height = 64
 				},
 				CommandParameter = shortcut,
-				Width = 64,
-				Padding = _defaultPadding
+				Width = 72
 			};
 			RenderOptions.SetBitmapScalingMode(newBtn, BitmapScalingMode.Fant);
 			newBtn.Click += ShortcutClicked;
@@ -99,25 +98,21 @@ namespace Dock
 			DockPanel.Children.Add(newBtn);
 		}
 
-		private void ButtonMouseLeave(object sender, MouseEventArgs e)
+		private static void ButtonMouseLeave(object sender, MouseEventArgs e)
 		{
-			var swellAnimation = new ThicknessAnimation(_mouseOverPadding, _defaultPadding, _swellDuration);
 			var button = (Button) sender;
-			button.BeginAnimation(PaddingProperty, swellAnimation);
 			((DropShadowEffect) ((Image) button.Content).Effect).Opacity = 0.0;
 		}
 
-		private void ButtonMouseEnter(object sender, MouseEventArgs e)
+		private static void ButtonMouseEnter(object sender, MouseEventArgs e)
 		{
-			var swellAnimation = new ThicknessAnimation(_defaultPadding, _mouseOverPadding, _swellDuration);
 			var button = (Button) sender;
-			button.BeginAnimation(PaddingProperty, swellAnimation);
 			((DropShadowEffect) ((Image) button.Content).Effect).Opacity = 0.2;
 		}
 
 		protected override void OnMouseEnter(MouseEventArgs e)
 		{
-			var popupAnimation = new DoubleAnimation(-64, 0, _popupDuration);
+			var popupAnimation = new DoubleAnimation(HiddenPosition, 0, _popupDuration);
 			BeginAnimation(TopProperty, popupAnimation);
 
 			base.OnMouseEnter(e);
@@ -125,7 +120,7 @@ namespace Dock
 
 		protected override void OnMouseLeave(MouseEventArgs e)
 		{
-			var popupAnimation = new DoubleAnimation(0, -64, _popupDuration);
+			var popupAnimation = new DoubleAnimation(0, HiddenPosition, _popupDuration);
 			BeginAnimation(TopProperty, popupAnimation);
 
 			base.OnMouseLeave(e);
